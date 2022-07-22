a9da1623f107e3f5530f7b6a174e9eb2ce6e9557
jun.kondo <kondo@test.com>
2022-07-22 15:44:42 +0900

    Changes to be committed: 	modified:   src/app/main/main.component.css 	modified:   src/app/main/main.component.html 	modified:   src/app/uploader/uploader.component.css 	modified:   src/app/uploader/uploader.component.html 	modified:   src/app/uploader/uploader.component.ts 	new file:   src/assets/file_upload_black_24dp.svg

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild,Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpEvent, HttpEventType, HttpHeaders, HttpParams, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-uploader',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploaderComponent implements OnInit {
 
-  constructor() { }
+  currentFile?: File;
+  progress = 0;
+  message = '';
+  fd? : FormData;
+
+  fileName = 'Select File';
+  fileInfos?: Observable<any>;
+
+  constructor(
+    private http: HttpClient,
+    private domSanitizer: DomSanitizer,
+    private matIconRegistry: MatIconRegistry,
+    ) {
+      this.matIconRegistry.addSvgIcon(
+        'file_upload',
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/file_upload_black_24dp.svg'));
+    }
 
   ngOnInit(): void {
   }
 
+  selectFile(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file: File = event.target.files[0];
+      this.currentFile = file;
+      this.fileName = this.currentFile.name;
+      this.fd = new FormData();
+      this.fd.append("upfile",this.currentFile);
+      //this.fddd: FormData = new FormData();
+    } else {
+      this.fileName = "Select File                           ";
+    }
+  }
+
+  upload(): void {
+    this.progress = 0;
+    this.message = "";
+    alert("upload!!!");
+  }
 }
