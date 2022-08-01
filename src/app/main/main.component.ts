ba29cb880289451eef78080b6d72e2d8fe1b50d2
jun.kondo <kondo@test.com>
2022-08-01 19:36:33 +0900

    Changes to be committed: 	modified:   README.md 	modified:   src/app/main/main.component.html 	modified:   src/app/main/main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -20,4 +20,18 @@ export class MainComponent implements OnInit {
     this.commonService.func01();
   }
 
+  oneDriveConnect(){
+    console.log("oneDriveConnect clicked");
+    //const target_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ad09f3e6-f94d-483b-b1a3-3bc5fa0d7b1a&scope=offline_access%20files.readwrite.all&response_type=code&redirect_uri=https%3A%2F%2Fmedia.plant11.com%2F";
+    //const target_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=4e55f9b5-c3e4-476f-96a9-1c002b84f90a&scope=offline_access%20files.readwrite.all&response_type=code&redirect_uri=http://localhost:4200/";
+    const target_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?"
+    + "client_id=4e55f9b5-c3e4-476f-96a9-1c002b84f90a"
+    + "&scope=offline_access%20files.readwrite.all"
+    + "&response_type=code"
+    + "&redirect_uri=http://localhost:4200/";
+
+    console.log("url:" + target_url);
+    
+    location.href=target_url;
+  }
 }
